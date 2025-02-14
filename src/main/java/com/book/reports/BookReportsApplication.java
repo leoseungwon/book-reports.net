60ed8f3ee6328e8b2dfc3b784d0d61e2604c6417
Seungwon Lee <misi1234567@naaver.com>
2025-02-14 09:41:35 +0900

    created project

---
+++ src/main/java/com/book/reports/BookReportsApplication.java
@@ -0,0 +1,13 @@
+package com.book.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookReportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/book/reports/BookReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
